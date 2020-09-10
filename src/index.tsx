9a265ae5eb706793970857bbbd2c29fc77314a80
MichaelJCompton <michael@dgraph.io>
2020-09-10 23:05:21 +1000

    squashed changes with auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,71 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
 import { App } from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "semantic-ui-css/semantic.min.css"
+import "./index.css";
+import './styles/main.css';
+
 import {
   ApolloClient,
-  InMemoryCache,
-  HttpLink,
   ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
 } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react"
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:8080/graphql",
-  }),
-})
+const AuthorizedApolloProvider: React.FC = ({ children }) => {
+  const { isAuthenticated, getIdTokenClaims } = useAuth0()
+
+  const httpLink = createHttpLink({
+    uri: process.env.REACT_APP_SLASH_GRAPHQL_ENDPOINT + "/graphql",
+  })
+
+  const authLink = setContext(async (_, { headers }) => {
+    if (!isAuthenticated) {
+      return { headers }
+    }
+
+    const token = await getIdTokenClaims()
+
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? token.__raw : "",
+      },
+    }
+  })
+
+  const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            queryPost: {
+              // Short for always preferring incoming over existing data.
+              merge: false,
+            },
+          },
+        },
+      },
+    }),
+  });
+
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+}
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN ?? "ERR"}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? "ERR"}
+    redirectUri={window.location.origin}
+  >
+    <AuthorizedApolloProvider>
+      <App />
+    </AuthorizedApolloProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 )
 
