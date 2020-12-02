8d488e8c9bbccaa96c88fc49860021c493f1afca
Apoorv Vardhan <vardhanapoorv@gmail.com>
2020-12-02 17:03:58 +0530

    Routing in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from "react"
-import "./App.css"
-import { Image } from "semantic-ui-react"
+import React from "react";
+import "./App.css";
+import { Image } from "semantic-ui-react";
+import { Home } from "./components/home";
+import { Post } from "./components/post";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export function App() {
   return (
@@ -22,8 +25,14 @@ export function App() {
             Learn about building GraphQL apps with Slash GraphQL at
             https://dgraph.io/learn
           </p>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/post/:id" component={Post} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </BrowserRouter>
         </div>
       </div>
     </>
-  )
+  );
 }
---
+++ src/components/post.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Post() {
+  return <div>Post</div>;
+}
