5dd5631ac1692feb23d8fd24fb2e65e69fa9225a
Abu Sakib <mabusakib@gmail.com>
2021-03-02 15:35:44 +0600

    Add Auth0 integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,69 @@ import ReactDOM from "react-dom"
 import { App } from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "semantic-ui-css/semantic.min.css"
-import "./index.css"
-import "./styles/main.css"
+import "./index.css";
+import './styles/main.css';
+
 import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react"
 
-const createApolloClient = () => {
+const AuthorizedApolloProvider: React.FC = ({ children }) => {
+  const { isAuthenticated, getIdTokenClaims } = useAuth0()
+
   const httpLink = createHttpLink({
-    uri: "<<Slash-GraphQL-URL>>",
+    uri: process.env.REACT_APP_SLASH_GRAPHQL_ENDPOINT + "/graphql",
   })
 
-  return new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
+  const authLink = setContext(async (_, { headers }) => {
+    if (!isAuthenticated) {
+      return { headers }
+    }
+
+    const token = await getIdTokenClaims()
+
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? token.__raw : "",
+      },
+    }
   })
+
+  const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            queryPost: {
+              // Short for always preferring incoming over existing data.
+              merge: false,
+            },
+          },
+        },
+      },
+    }),
+  });
+
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
 }
 
 ReactDOM.render(
-  <ApolloProvider client={createApolloClient()}>
-    <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN ?? "ERR"}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? "ERR"}
+    redirectUri={window.location.origin}
+  >
+    <AuthorizedApolloProvider>
       <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+    </AuthorizedApolloProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 )
 
